c3b4ed645b60140d7a114c53853b4d2d0bd5c7c0
William Boles <william@memrise.com>
2016-10-30 11:33:45 +0000

    Basic ghost typing example

--- GhostTyping-Example/Stories/FirstViewController.swift
+++ GhostTyping-Example/Stories/FirstViewController.swift
@@ -10,16 +10,64 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet weak var typingLabel: UILabel!
+    @IBOutlet weak var animatorButton: UIButton!
+    
+    var animationTimer: Timer?
+    var fullTextToBeWritten: String?
+    
+    // MARK: - ViewLifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        fullTextToBeWritten = typingLabel.text
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        animationTimer?.invalidate()
     }
-
-
+    
+    // MARK: - ButtonActions
+    
+    @IBAction func animatorButtonPressed(_ sender: Any) {
+        animateText()
+    }
+    
+    // MARK: Animation
+    
+    func animateText() {
+        animationTimer?.invalidate()
+        typingLabel.text = nil
+        
+        var nextCharacterIndexToBeShown = 0
+        
+        animationTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { [weak self] (timer: Timer) in
+            if let fullTextToBeWritten = self?.fullTextToBeWritten {
+                let characters = Array(fullTextToBeWritten.characters)
+                
+                if nextCharacterIndexToBeShown < characters.count {
+                    if let label = self?.typingLabel {
+                        let nextCharacterToAdd = String(characters[nextCharacterIndexToBeShown])
+                        
+                        if let currentText = label.text {
+                            label.text = currentText + nextCharacterToAdd
+                        } else {
+                            label.text = nextCharacterToAdd
+                        }
+                    }
+                    
+                    nextCharacterIndexToBeShown += 1
+                } else {
+                    timer.invalidate()
+                }
+            } else {
+                timer.invalidate()
+            }
+        })
+    }
 }
 
