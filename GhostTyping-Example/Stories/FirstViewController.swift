52598411179732c19a4a29314d8ae8fe7f0c7cdb
William Boles <william@memrise.com>
2016-10-30 10:53:26 +0000

    Initial project structure

---
+++ GhostTyping-Example/Stories/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GhostTyping-Example
+//
+//  Created by William Boles on 30/10/2016.
+//  Copyright © 2016 Boles. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
